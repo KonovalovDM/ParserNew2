b66b57ff56c6bb30eac213add94537b71ccaa32b
KonovalovDM <kdm232006@gmail.com>
2024-10-23 23:25:51 +0300

    add code

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ import time
 # С помощью time мы можем делать задержки в программе
 
 browser = webdriver.Firefox()  # Firefox(),
-browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
 assert "Википедия" in browser.title
 time.sleep(2)
 
-serch_box = browser.find_element(By.ID,"searchInput")
-serch_box.send_keys("Солнечная система")
+paragraphs = browser.find_elements(By.TAG_NAME,"p")
+for paragraph in paragraphs:
+    print(paragraph.text)
+    input()
+
+# serch_box = browser.find_element(By.ID,"searchInput")
+# serch_box.send_keys("Солнечная система")
 # time.sleep(2)
-serch_box.send_keys(Keys.RETURN)
-time.sleep(5)
-a = browser.find_element(By.LINK_TEXT,"Солнечная система").click()
-time.sleep(5)
-browser.quit()  # закрываем браузер
+# serch_box.send_keys(Keys.RETURN)
+# time.sleep(5)
+# a = browser.find_element(By.LINK_TEXT,"Солнечная система").click()
+# time.sleep(5)
+# browser.quit()  # закрываем браузер
 
 # browser.save_screenshot("dom.png")
 # time.sleep(4) # задержка в 15 секунд
@@ -27,4 +32,5 @@ browser.quit()  # закрываем браузер
 # time.sleep(3)
 # browser.refresh()
 # time.sleep(5)
-# browser.quit()  # закрываем браузер
+
+browser.quit()  # закрываем браузер
